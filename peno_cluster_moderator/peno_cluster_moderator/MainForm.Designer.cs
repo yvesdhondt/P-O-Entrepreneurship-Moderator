ff4251bb681fc20f2df41b39c45314db7d81f383
Yves D'hondt <yves.dhondt12@gmail.com>
2020-03-27 02:42:25 +0100

    ...

--- peno_cluster_moderator/peno_cluster_moderator/MainForm.Designer.cs
+++ peno_cluster_moderator/peno_cluster_moderator/MainForm.Designer.cs
@@ -60,7 +60,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(401, 346);
             this.Controls.Add(this.btnBlacklist);
             this.Controls.Add(this.btnReportedQA);
             this.Name = "MainForm";
